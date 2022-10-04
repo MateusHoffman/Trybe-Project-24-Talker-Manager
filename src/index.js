bebbc4a849298f4c6ef05ca8e1c064a97b9bdfd1
MateusHoffman <mateushoffmandev@gmail.com>
2022-10-04 15:27:38 -0300

    3 done

--- src/index.js
+++ src/index.js
@@ -15,19 +15,37 @@ app.get('/', (_request, response) => {
 
 // START
 
+function tokenRandom(tamanho) {
+  let stringAleatoria = '';
+  const caracteres = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  for (let i = 0; i < tamanho; i += 1) {
+      stringAleatoria += caracteres.charAt(Math.floor(Math.random() * caracteres.length));
+  }
+  return stringAleatoria;
+}
+
 // GET all speakers
-app.get('/talker', async (req, res) => {
+app.get('/talker', (req, res) => {
   const arrSpeakers = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
   res.status(200).json(arrSpeakers);
 });
 
 // GET one speaker
-app.get('/talker/:id', async (req, res) => {
+app.get('/talker/:id', (req, res) => {
   const arrSpeakers = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
   const objSpeaker = arrSpeakers.find((speaker) => speaker.id === Number(req.params.id));
   
   if (objSpeaker) return res.status(200).json(objSpeaker);
   return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+});
+
+// POST login
+app.post('/login', (req, res) => {
+  const { email, password } = req.body;
+  fs.writeFileSync('src/talker.json', JSON.stringify({ email, password }));
+
+  const token = tokenRandom(16);
+  return res.status(200).json({ token });
 });
 
 // END
