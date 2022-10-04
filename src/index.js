b2c9be55ef49fad5297d5d8836ddd97dcafab517
MateusHoffman <mateushoffmandev@gmail.com>
2022-10-04 15:01:33 -0300

    2 done

--- src/index.js
+++ src/index.js
@@ -17,14 +17,17 @@ app.get('/', (_request, response) => {
 
 // GET all speakers
 app.get('/talker', async (req, res) => {
-  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
-  res.status(200).json(data);
+  const arrSpeakers = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
+  res.status(200).json(arrSpeakers);
 });
 
-// GET all speakers
-app.get('/talker', async (req, res) => {
-  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
-  res.status(200).json(data);
+// GET one speaker
+app.get('/talker/:id', async (req, res) => {
+  const arrSpeakers = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
+  const objSpeaker = arrSpeakers.find((speaker) => speaker.id === Number(req.params.id));
+  
+  if (objSpeaker) return res.status(200).json(objSpeaker);
+  return res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
 });
 
 // END
