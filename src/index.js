3a9f4b9f7461f876e0bf2ce214fbab9ff461f8af
MateusHoffman <mateushoffmandev@gmail.com>
2022-10-04 14:54:30 -0300

    1 done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,6 +12,22 @@ const PORT = '3000';
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
 });
+
+// START
+
+// GET all speakers
+app.get('/talker', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
+  res.status(200).json(data);
+});
+
+// GET all speakers
+app.get('/talker', async (req, res) => {
+  const data = JSON.parse(fs.readFileSync('src/talker.json', 'utf8'));
+  res.status(200).json(data);
+});
+
+// END
 
 app.listen(PORT, () => {
   console.log('Online');
